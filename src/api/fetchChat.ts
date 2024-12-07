c004932a9c4fddc983901ae3a0ba4dbed505322e
Matthias Hermsen <matthiashermsen@gmail.com>
2024-12-07 16:17:20 +0100

    fix: use URL constructor when fetching chat (#2)

--- src/api/fetchChat.ts
+++ src/api/fetchChat.ts
@@ -10,7 +10,9 @@ async function fetchChat({
 		content: string;
 	}[];
 }) {
-	const url = `https://api.infomaniak.com/1/ai/${productId}/openai/chat/completions`;
+	const url = new URL(
+		`https://api.infomaniak.com/1/ai/${productId}/openai/chat/completions`,
+	);
 
 	const headers = new Headers();
 	headers.set("Authorization", `Bearer ${token}`);
